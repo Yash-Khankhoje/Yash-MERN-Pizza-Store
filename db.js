023601a6d5b4dcf32454d9121bc6111c9b311a3e
Yash-Khankhohe <yash.khankhoje@gmail.com>
2021-10-04 15:45:22 +0530

    First try.

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoURL = 'mongodb+srv://nsvy:Test4689@cluster0.5kgho.mongodb.net/mern-pizza'
+
+mongoose.connect(mongoURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var db = mongoose.connection
+
+db.on('connected' , ()=>{
+    console.log('Mongo DB Connection Successfull');
+})
+
+db.on('error' , ()=>{
+    console.log(`Mongo DB Connection failed`);
+})
+
+module.exports =mongoose
